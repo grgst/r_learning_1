48772ad99e17ec250b7a947c83bb6d1822f4f1d1
Greg <grgst@users.noreply.github.com>
2018-10-13 16:26:34 +0200

    added more plots introduced questions that plots answer

--- part1.R
+++ part1.R
@@ -6,6 +6,10 @@
 require(tidyverse)
 require(lintr)
 
+#' Load dara
+mpg <-  mpg
+
+#' Explore data'
 summary(mpg)
 ?mpg
 
@@ -23,16 +27,39 @@ myplot1 + theme_minimal()
 #' 2nd graph overlaid to show the difference
 myplot1a <- ggplot(data = mpg) +
   geom_point(mapping = aes(x = mpg$displ, y = mpg$hwy), alpha = 1/2) +
-  geom_point(mapping = aes(x = mpg$displ, y = mpg$hwy), 
+  geom_point(mapping = aes(x = mpg$displ, y = mpg$hwy),
              position = "jitter", color = "red", alpha = 1/4)
 
 myplot1a + theme_bw()
 
+#' Have cars improved in fuel efficiency from 1999 to 2008?
+#' Color cars from 1999 in red, from 2008 in green
+#' //TODO: refactor, only works with short variables 
+#' (without data frame name prefixed)
+
+myplot1x <-  ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(data = filter(mpg, year == "1999"), color = "red") +
+  geom_point(data = filter(mpg, year == "2008"), color = "green")
+
+#' Add labels...
+myplot1x[["labels"]][["x"]] <- "Engine displacement (l)"
+myplot1x[["labels"]][["y"]] <- "Fuel efficiency highway (mpg)"
+
+myplot1x + theme_bw()
+
+#' How do cars city and highway fuel efficiency rates compare?
+#' City (red) vs. highway (blue)
+myplot1y <-  ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy), color = "blue") +
+  geom_point(mapping = aes(x = displ, y = cty), color = "red")
+
+myplot1y + theme_bw()
+
 
 #' (2) Plot with color
 myplot2 <-  ggplot(data = mpg) +
-  geom_point(mapping = aes(x = mpg$displ, 
-                           y = mpg$hwy, 
+  geom_point(mapping = aes(x = mpg$displ,
+                           y = mpg$hwy,
                            color = mpg$class)
              )
 
@@ -43,8 +70,8 @@ myplot2 + theme_classic()
 #' (3) PLot
 #' 
 myplot3 <- ggplot(data = mpg) +
-  geom_point(mapping = aes(x = mpg$displ, 
-                           y = mpg$hwy, 
+  geom_point(mapping = aes(x = mpg$displ,
+                           y = mpg$hwy,
                            color = mpg$manufacturer)
              )
 
@@ -67,7 +94,7 @@ myplot4 + theme_classic()
 #' (5) PLot with Facets, Part 2
 #' Grouped by cylinders (cyl)
 #' 
-myplot5 <- ggplot(data = mpg) + 
+myplot5 <- ggplot(data = mpg) +
   geom_point(mapping = aes(x = mpg$displ, y = mpg$hwy)) +
   facet_grid(. ~ mpg$cyl)
 
@@ -80,10 +107,10 @@ myplot5 + theme_light()
 
 #' (6) PLot 
 #' 
-myplot6 <- ggplot(data = mpg) + 
+myplot6 <- ggplot(data = mpg) +
   geom_point(mapping = aes(x = mpg$displ, y = mpg$hwy)) +
-  geom_smooth(mapping = aes(x = mpg$displ, 
-                            y = mpg$hwy, 
+  geom_smooth(mapping = aes(x = mpg$displ,
+                            y = mpg$hwy,
                             color = mpg$drv
                             )
               )
@@ -94,3 +121,4 @@ myplot6[["labels"]][["x"]] <- "Displacement"
 myplot6[["labels"]][["y"]] <- "Fuel Consumption Highway (mpg)"
 
 myplot6 + theme_light()
+
