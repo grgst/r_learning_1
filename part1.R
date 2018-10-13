fdfba3f3563d3d1f711c7a7e4ce038210a2e8d48
Greg <grgst@users.noreply.github.com>
2018-10-13 11:35:02 +0200

    ggplot experiments

--- part1.R
+++ part1.R
@@ -3,11 +3,82 @@
 require(tidyverse)
 require(lintr)
 
-mpg
+mpg <- mpg
 
 #' (1) Plot
 #' 
-ggplot(data = mpg) +
-  geom_point(mapping = aes(x = displ, y = hwy))
+myplot1 <- ggplot(data = mpg) +
+  geom_point(mapping = aes(x = mpg$displ, y = mpg$hwy))
+
+myplot1 + theme_dark()
+myplot1 + theme_bw()
+myplot1 + theme_light()
+myplot1 + theme_minimal()
+
+#' (2) Plot with color
+myplot2 <-  ggplot(data = mpg) +
+  geom_point(mapping = aes(x = mpg$displ, 
+                           y = mpg$hwy, 
+                           color = mpg$class)
+             )
+
+myplot2
+myplot2 + theme_classic()
+
+
+#' (3) PLot
+#' 
+myplot3 <- ggplot(data = mpg) +
+  geom_point(mapping = aes(x = mpg$displ, 
+                           y = mpg$hwy, 
+                           color = mpg$manufacturer)
+             )
+
+#' Change Labels by saving plots in variables, and edit in RStudio
+myplot3[["labels"]][["x"]] <- "Displacement"
+myplot3[["labels"]][["y"]] <- "Fuel Consumption Highway (mpg)"
+myplot3[["labels"]][["colour"]] <- "Manufacturer"
+
+myplot3 + theme_classic()
+
+#' (4) Plot with facets
+#' Grouped by drivetrain (drv)
+#' 
+myplot4 <-  ggplot(data = mpg) +
+  geom_point(mapping = aes(x = mpg$displ, y = mpg$hwy)) +
+  facet_grid(mpg$drv ~ .)
+
+myplot4 + theme_classic()
+
+#' (5) PLot with Facets, Part 2
+#' Grouped by cylinders (cyl)
+#' 
+myplot5 <- ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = mpg$displ, y = mpg$hwy)) +
+  facet_grid(. ~ mpg$cyl)
+
+#' Assign labels...
+myplot5[["labels"]][["x"]] <- "Displacement"
+myplot5[["labels"]][["y"]] <- "Fuel Consumption Highway (mpg)"
+
+myplot5 + theme_linedraw()
+myplot5 + theme_light()
+
+#' (6) PLot 
+#' 
+myplot6 <- ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = mpg$displ, y = mpg$hwy)) +
+  geom_smooth(mapping = aes(x = mpg$displ, 
+                            y = mpg$hwy, 
+                            color = mpg$drv
+                            )
+              )
+
+#' Assign labels... 
+#' //TODO: refactor
+myplot6[["labels"]][["x"]] <- "Displacement"
+myplot6[["labels"]][["y"]] <- "Fuel Consumption Highway (mpg)"
+
+myplot6 + theme_light()
 
   
